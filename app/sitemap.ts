d2ce3b12b65d4642693b8dbb1fcac39ed3294ec7
vanessaliu036-lab <vanessaliu036@gmail.com>
2026-09-13 14:55:28 +0800

    feat: add Partnerships to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -35,6 +35,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     { url: `${siteUrl}/solutions/roasting-program`, lastModified: now },
     { url: `${siteUrl}/solutions/coffee-marketing`, lastModified: now },
     { url: `${siteUrl}/solutions/equipment-service`, lastModified: now },
+    { url: `${siteUrl}/partnerships`, lastModified: now },
     { url: `${siteUrl}/distribution`, lastModified: now },
 
     { url: `${siteUrl}/blog`, lastModified: now },
