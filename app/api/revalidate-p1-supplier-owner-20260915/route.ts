708fae0129587d2482431a7be6992367ab1c027e
vanessaliu036-lab <vanessaliu036@gmail.com>
2026-09-15 12:01:14 +0800

    chore(seo): remove one-time Supplier Owner revalidation

--- app/api/revalidate-p1-supplier-owner-20260915/route.ts
+++
@@ -1,9 +0,0 @@
-import { revalidatePath } from 'next/cache'
-
-const SUPPLIER_OWNER_PATH =
-  '/blog/evaluating-cambodian-coffee-suppliers-a-procurement-manager-s-guide-to-quality-and-traceability'
-
-export async function GET() {
-  revalidatePath(SUPPLIER_OWNER_PATH)
-  return Response.json({ revalidated: true, path: SUPPLIER_OWNER_PATH })
-}
