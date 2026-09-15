fa7d3213befcfe3c57ea52c153d6293413e0cf42
vanessaliu036-lab <vanessaliu036@gmail.com>
2026-09-15 12:10:16 +0800

    chore(seo): remove one-time Mondulkiri owner revalidation

--- app/api/revalidate-p1-mondulkiri-owner-20260915/route.ts
+++
@@ -1,8 +0,0 @@
-import { revalidatePath } from 'next/cache'
-
-const MONDULKIRI_OWNER_PATH = '/blog/mondulkiri-next-specialty-coffee-origin'
-
-export async function GET() {
-  revalidatePath(MONDULKIRI_OWNER_PATH)
-  return Response.json({ revalidated: true, path: MONDULKIRI_OWNER_PATH })
-}
