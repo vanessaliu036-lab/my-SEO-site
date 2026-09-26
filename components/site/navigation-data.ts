a8b052ce672770473d0895c4abe3fcc0d16387fe
vanessaliu036-lab <vanessaliu036@gmail.com>
2026-09-26 16:48:23 +0800

    Add hotel program to Solutions navigation

--- components/site/navigation-data.ts
+++ components/site/navigation-data.ts
@@ -35,6 +35,7 @@ export const siteNavigation: readonly SiteNavItem[] = [
     children: [
       { label: "Wholesale", href: "/solutions/wholesale" },
       { label: "Roasting Program", href: "/solutions/roasting-program" },
+      { label: "Hotel Coffee Program", href: "/solutions/hotels" },
       { label: "Coffee Marketing", href: "/solutions/coffee-marketing" },
     ],
   },
